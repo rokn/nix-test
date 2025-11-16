ad83230faaf656b270ea4c111db516d72d591db0
Antonio Mindov <amindov98@gmail.com>
2025-11-16 17:20:57 +0200

    Initial

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # nix-test
+
+## Development shell
+
+This repository uses a Nix flake to provide a reproducible shell environment that includes `git`.
+
+### Requirements
+
+- Nix with flake support enabled.
+
+### Usage
+
+```
+nix develop
+```
+
+This will drop you into a shell that has `git` available. Leave the shell with `exit`.
---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  description = "Nix Based reproducible development environment";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            git
+          ];
+        };
+      });
+}
